af75770c435c5e11dbecf9f8fc2ded09ea8e63a4
John Pertell <pertelj@sunypoly.edu>
2024-01-29 11:53:24 -0500

    Added Requirements/Versions

---
+++ check_versions.py
@@ -0,0 +1,20 @@
+import sys
+import tensorflow as tf
+import cv2
+import numpy as np
+import matplotlib
+
+python_version = sys.version
+
+tf_version = tf.__version__
+cv_version = cv2.__version__
+np_version = np.__version__
+matplotlib_version = matplotlib.__version__
+
+print("===VERSIONS===")
+print("Python Version:", python_version)
+print("TensorFlow Version:", tf_version)
+print("OpenCV version:", cv_version)
+print("Numpy Version:", np_version)
+print("Matplotlib Version:", matplotlib_version)
+print("==============")
