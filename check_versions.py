e25d3a64c3f37918959c55b36efdb773f5a1351d
Johnny <johnangus425@gmail.com>
2024-02-01 20:29:46 -0500

    switch to pytorch2

--- check_versions.py
+++ check_versions.py
@@ -1,5 +1,5 @@
 import sys
-import tensorflow as tf
+import torch
 import cv2
 import numpy as np
 import matplotlib
@@ -8,14 +8,15 @@ from PyQt5.QtCore import QT_VERSION_STR, PYQT_VERSION_STR
 
 python_version = sys.version
 
-tf_version = tf.__version__
+pyTorch_version = torch.__version__
 cv_version = cv2.__version__
 np_version = np.__version__
 matplotlib_version = matplotlib.__version__
 
-print("===VERSIONS===")
+print("YOUR PYTHON VERSIONS:")
+print("==============")
 print("Python Version:", python_version)
-print("TensorFlow Version:", tf_version)
+print("PyTorch Version:", pyTorch_version)
 print("OpenCV version:", cv_version)
 print("Numpy Version:", np_version)
 print("Matplotlib Version:", matplotlib_version)
